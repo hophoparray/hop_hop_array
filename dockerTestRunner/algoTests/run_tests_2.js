a13c0a01aed919368e0e672727ccdd1e8b058bcf
Becca Duhamel <rduhamel9@gmail.com>
2020-02-03 12:44:19 -0500

    Feature: algo tests

--- dockerTestRunner/algoTests/run_tests_2.js
+++ dockerTestRunner/algoTests/run_tests_2.js
@@ -1,6 +1,30 @@
-// for remove duplicates algo
+// algo: sortedSquares
 
-/*
-'Test.assertDeepEquals(removeDuplicatesSet([5, 8], 0),[8])',
-        'Test.assertDeepEquals(removeDuplicatesSet([5, 8, 3, 2, 7, 1, 4, 9, 15, 30], 8),[5, 8, 3, 2, 7, 1, 4, 9, 30])'
-*/
+var assert = require('assert')
+
+// console.log("Importing file...")
+var usersCode = require('./usersCode')
+// console.log(usersCode)
+
+let testResults = []
+
+// console.log("Starting tests...")
+
+let testName = 'test1'
+try {
+  assert(usersCode.sortedSquares([-4, -1, 0, 3, 10]) == [0, 1, 9, 16, 100])
+} catch {
+  testResults.push(testName)
+}
+
+testName = 'test2'
+try {
+  assert(usersCode.sortedSquares([-7, -3, 2, 3, 11]) == [[4, 9, 9, 49, 121]])
+} catch (error) {
+  testResults.push(testName)
+}
+
+// console.log("If you are reading this, you have passed all the tests!")
+if (testResults.length > 0) {
+  console.log(testResults)
+}
--- dockerTestRunner/algoTests/run_tests_4.js
+++ dockerTestRunner/algoTests/run_tests_4.js
@@ -1,7 +1,37 @@
-// for is palindrome algo
+// algo: isPalindromStack
 
-/*
- 'Test.assertDeepEquals(isPalindromeStack([1, 2, 1]),true)',
-        'Test.assertDeepEquals(isPalindromeStack([2, 1, 3, 3, 1, 2]),true)',
-        'Test.assertDeepEquals(isPalindromeStack([1, 2, 2]),false)'
-*/
+var assert = require('assert')
+
+// console.log("Importing file...")
+var usersCode = require('./usersCode')
+// console.log(usersCode)
+
+let testResults = []
+
+// console.log("Starting tests...")
+
+let testName = 'test1'
+try {
+  assert(usersCode.isPalindromStack([1, 2, 1]) == true)
+} catch {
+  testResults.push(testName)
+}
+
+testName = 'test2'
+try {
+  assert(usersCode.isPalindromStack([2, 1, 3, 3, 1, 2]) == true)
+} catch (error) {
+  testResults.push(testName)
+}
+
+testName = 'test3'
+try {
+  assert(usersCode.isPalindromStack([1, 2, 2]) == false)
+} catch (error) {
+  testResults.push(testName)
+}
+
+// console.log("If you are reading this, you have passed all the tests!")
+if (testResults.length > 0) {
+  console.log(testResults)
+}
