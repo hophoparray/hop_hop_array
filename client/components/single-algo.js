039d8ed4b2625a165ea116d9e0eb92c186315059
Avelina Kim <avelina@gmail.com>
2020-02-03 15:36:04 -0500

    refactor: try to get the value of editor

--- client/components/single-algo.js
+++ client/components/single-algo.js
@@ -12,23 +12,46 @@ class SingleAlgo extends React.Component {
       userCode: '',
       failedTests: [],
       passingAllTests: false,
-      currentAlgo: {}
+      currentAlgo: {},
+      editorMounted: false
     }
     this.editorDidMount = this.editorDidMount.bind(this)
     this.onAttempt = this.onAttempt.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
   editorDidMount(editor, monaco) {
-    console.log('editorDidMount', editor)
+    console.log('editorDidMount')
     console.log(this.props.match)
+    this.editor = editor
     // this.props.match.params.id => get algo id
   }
-  onAttempt() {}
+  componentDidUpdate() {
+    console.log('UPDATE')
+  }
+  onAttempt(value) {
+    console.log('Attempt', value)
+  }
+  handleChange() {
+    console.log('Change')
+    this.setState({editorMounted: true})
+  }
 
   render() {
     const options = {selectOnLineNumbers: true}
+    const value = 0
+    if (this.state.editorMounted || Object.keys(this.refs).length) {
+      console.log('MOUNTED')
+      console.log('THIS', this)
+      const model = this.refs.monaco.editor.getModel()
+      const value = model.getValue()
+      console.log('MODEL', model)
+      console.log('VALUE', value)
+    }
+
     return (
       <div>
         <MonacoEditor
+          ref="monaco"
           monaco={monaco}
           width="800"
           height="400"
@@ -36,11 +59,12 @@ class SingleAlgo extends React.Component {
           theme="vs-dark"
           value={this.state.defaultText}
           options={options}
-          editorDidMount={this.editorDidMount()}
+          editorDidMount={this.editorDidMount}
+          onChange={this.handleChange}
         />
         {/* TO DO: Add Submit button when tests pass */}
         <button>Give me a {'<br/>'}</button>
-        <button>Attempt</button>
+        <button onClick={() => this.onAttempt(value)}>Attempt</button>
         {/* TODO: Continue flow to fail/succeed components */}
       </div>
     )
